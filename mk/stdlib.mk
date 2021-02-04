9e7aeebbf63677da4bffefcca5fb4dbded521937
Drew DeVault <sir@cmpwn.com>
2021-02-04 17:06:11 -0500

    Update stdlib

--- mk/stdlib.mk
+++ mk/stdlib.mk
@@ -319,7 +319,8 @@ testlib_io_srcs= \
 	$(STDLIB)/io/types.ha \
 	$(STDLIB)/io/copy.ha \
 	$(STDLIB)/io/println.ha \
-	$(STDLIB)/io/stream.ha 
+	$(STDLIB)/io/stream.ha \
+	$(STDLIB)/io/copy+test.ha 
 
 $(TESTCACHE)/io/io.ssa: $(testlib_io_srcs) $(testlib_rt)
 	@printf 'HAREC \t$@\n'
