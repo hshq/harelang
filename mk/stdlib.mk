3bee9a4d41eb1fb11c80ae84041d669c71dcbc0a
Eyal Sawady <ecs@d2evs.net>
2021-02-04 21:50:34 -0500

    gen-stdlib: gen_lib all modules upfront
    Fixes some weirdness requiring modules to be generated after their
    dependencies.
    
    Also alpha-sort modules now that that's possible.

--- mk/stdlib.mk
+++ mk/stdlib.mk
@@ -57,6 +57,33 @@ stdlib_rt=$(HARECACHE)/rt/rt.a
 stdlib_start=$(HARECACHE)/rt/start.o
 hare_stdlib_deps+=$(stdlib_rt)
 
+stdlib_ascii=$(HARECACHE)/ascii/ascii.o
+hare_stdlib_deps+=$(stdlib_ascii)
+
+stdlib_bytes=$(HARECACHE)/bytes/bytes.o
+hare_stdlib_deps+=$(stdlib_bytes)
+
+stdlib_encoding_utf8=$(HARECACHE)/encoding/utf8/encoding.utf8.o
+hare_stdlib_deps+=$(stdlib_encoding_utf8)
+
+stdlib_fmt=$(HARECACHE)/fmt/fmt.o
+hare_stdlib_deps+=$(stdlib_fmt)
+
+stdlib_io=$(HARECACHE)/io/io.o
+hare_stdlib_deps+=$(stdlib_io)
+
+stdlib_os=$(HARECACHE)/os/os.o
+hare_stdlib_deps+=$(stdlib_os)
+
+stdlib_strconv=$(HARECACHE)/strconv/strconv.o
+hare_stdlib_deps+=$(stdlib_strconv)
+
+stdlib_strings=$(HARECACHE)/strings/strings.o
+hare_stdlib_deps+=$(stdlib_strings)
+
+stdlib_types=$(HARECACHE)/types/types.o
+hare_stdlib_deps+=$(stdlib_types)
+
 # ascii
 stdlib_ascii_srcs= \
 	$(STDLIB)/ascii/ctype.ha 
@@ -66,9 +93,6 @@ $(HARECACHE)/ascii/ascii.ssa: $(stdlib_ascii_srcs) $(stdlib_rt)
 	@mkdir -p $(HARECACHE)/ascii
 	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nascii \
 		-t$(HARECACHE)/ascii/ascii.td $(stdlib_ascii_srcs)
-
-stdlib_stdlib_ascii=$(HARECACHE)/ascii/ascii.o
-hare_stdlib_deps+=$(stdlib_stdlib_ascii)
 
 # bytes
 stdlib_bytes_srcs= \
@@ -83,23 +107,27 @@ $(HARECACHE)/bytes/bytes.ssa: $(stdlib_bytes_srcs) $(stdlib_rt)
 	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nbytes \
 		-t$(HARECACHE)/bytes/bytes.td $(stdlib_bytes_srcs)
 
-stdlib_stdlib_bytes=$(HARECACHE)/bytes/bytes.o
-hare_stdlib_deps+=$(stdlib_stdlib_bytes)
+# encoding::utf8
+stdlib_encoding_utf8_srcs= \
+	$(STDLIB)/encoding/utf8/rune.ha \
+	$(STDLIB)/encoding/utf8/decode.ha \
+	$(STDLIB)/encoding/utf8/encode.ha 
 
-# types
-stdlib_types_srcs= \
-	$(STDLIB)/types/limits.ha \
-	$(STDLIB)/types/classes.ha \
-	$(STDLIB)/types/arch$(ARCH).ha 
-
-$(HARECACHE)/types/types.ssa: $(stdlib_types_srcs) $(stdlib_rt)
+$(HARECACHE)/encoding/utf8/encoding.utf8.ssa: $(stdlib_encoding_utf8_srcs) $(stdlib_rt) $(stdlib_types)
 	@printf 'HAREC \t$@\n'
-	@mkdir -p $(HARECACHE)/types
-	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Ntypes \
-		-t$(HARECACHE)/types/types.td $(stdlib_types_srcs)
+	@mkdir -p $(HARECACHE)/encoding/utf8
+	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nencoding::utf8 \
+		-t$(HARECACHE)/encoding/utf8/encoding.utf8.td $(stdlib_encoding_utf8_srcs)
 
-stdlib_stdlib_types=$(HARECACHE)/types/types.o
-hare_stdlib_deps+=$(stdlib_stdlib_types)
+# fmt
+stdlib_fmt_srcs= \
+	$(STDLIB)/fmt/fmt.ha 
+
+$(HARECACHE)/fmt/fmt.ssa: $(stdlib_fmt_srcs) $(stdlib_rt) $(stdlib_io) $(stdlib_os) $(stdlib_strconv) $(stdlib_strings) $(stdlib_types)
+	@printf 'HAREC \t$@\n'
+	@mkdir -p $(HARECACHE)/fmt
+	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nfmt \
+		-t$(HARECACHE)/fmt/fmt.td $(stdlib_fmt_srcs)
 
 # io
 stdlib_io_srcs= \
@@ -115,56 +143,6 @@ $(HARECACHE)/io/io.ssa: $(stdlib_io_srcs) $(stdlib_rt)
 	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nio \
 		-t$(HARECACHE)/io/io.td $(stdlib_io_srcs)
 
-stdlib_stdlib_io=$(HARECACHE)/io/io.o
-hare_stdlib_deps+=$(stdlib_stdlib_io)
-
-# encoding::utf8
-stdlib_encoding_utf8_srcs= \
-	$(STDLIB)/encoding/utf8/rune.ha \
-	$(STDLIB)/encoding/utf8/decode.ha \
-	$(STDLIB)/encoding/utf8/encode.ha 
-
-$(HARECACHE)/encoding/utf8/encoding.utf8.ssa: $(stdlib_encoding_utf8_srcs) $(stdlib_rt) $(stdlib_stdlib_types)
-	@printf 'HAREC \t$@\n'
-	@mkdir -p $(HARECACHE)/encoding/utf8
-	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nencoding::utf8 \
-		-t$(HARECACHE)/encoding/utf8/encoding.utf8.td $(stdlib_encoding_utf8_srcs)
-
-stdlib_stdlib_encoding_utf8=$(HARECACHE)/encoding/utf8/encoding.utf8.o
-hare_stdlib_deps+=$(stdlib_stdlib_encoding_utf8)
-
-# strings
-stdlib_strings_srcs= \
-	$(STDLIB)/strings/cstrings.ha \
-	$(STDLIB)/strings/dup.ha \
-	$(STDLIB)/strings/iter.ha \
-	$(STDLIB)/strings/utf8.ha 
-
-$(HARECACHE)/strings/strings.ssa: $(stdlib_strings_srcs) $(stdlib_rt) $(stdlib_stdlib_encoding_utf8) $(stdlib_stdlib_types)
-	@printf 'HAREC \t$@\n'
-	@mkdir -p $(HARECACHE)/strings
-	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nstrings \
-		-t$(HARECACHE)/strings/strings.td $(stdlib_strings_srcs)
-
-stdlib_stdlib_strings=$(HARECACHE)/strings/strings.o
-hare_stdlib_deps+=$(stdlib_stdlib_strings)
-
-# strconv
-stdlib_strconv_srcs= \
-	$(STDLIB)/strconv/itos.ha \
-	$(STDLIB)/strconv/utos.ha \
-	$(STDLIB)/strconv/stou.ha \
-	$(STDLIB)/strconv/numeric.ha 
-
-$(HARECACHE)/strconv/strconv.ssa: $(stdlib_strconv_srcs) $(stdlib_rt) $(stdlib_stdlib_types) $(stdlib_stdlib_strings) $(stdlib_stdlib_ascii)
-	@printf 'HAREC \t$@\n'
-	@mkdir -p $(HARECACHE)/strconv
-	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nstrconv \
-		-t$(HARECACHE)/strconv/strconv.td $(stdlib_strconv_srcs)
-
-stdlib_stdlib_strconv=$(HARECACHE)/strconv/strconv.o
-hare_stdlib_deps+=$(stdlib_stdlib_strconv)
-
 # os
 stdlib_os_srcs= \
 	$(STDLIB)/os/$(PLATFORM)/environ.ha \
@@ -176,27 +154,49 @@ stdlib_os_srcs= \
 	$(STDLIB)/os/environ.ha \
 	$(STDLIB)/os/stdfd.ha 
 
-$(HARECACHE)/os/os.ssa: $(stdlib_os_srcs) $(stdlib_rt) $(stdlib_stdlib_strings) $(stdlib_stdlib_types)
+$(HARECACHE)/os/os.ssa: $(stdlib_os_srcs) $(stdlib_rt) $(stdlib_strings) $(stdlib_types)
 	@printf 'HAREC \t$@\n'
 	@mkdir -p $(HARECACHE)/os
 	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nos \
 		-t$(HARECACHE)/os/os.td $(stdlib_os_srcs)
 
-stdlib_stdlib_os=$(HARECACHE)/os/os.o
-hare_stdlib_deps+=$(stdlib_stdlib_os)
+# strconv
+stdlib_strconv_srcs= \
+	$(STDLIB)/strconv/itos.ha \
+	$(STDLIB)/strconv/utos.ha \
+	$(STDLIB)/strconv/stou.ha \
+	$(STDLIB)/strconv/numeric.ha 
 
-# fmt
-stdlib_fmt_srcs= \
-	$(STDLIB)/fmt/fmt.ha 
-
-$(HARECACHE)/fmt/fmt.ssa: $(stdlib_fmt_srcs) $(stdlib_rt) $(stdlib_stdlib_io) $(stdlib_stdlib_os) $(stdlib_stdlib_strconv) $(stdlib_stdlib_strings) $(stdlib_stdlib_types)
+$(HARECACHE)/strconv/strconv.ssa: $(stdlib_strconv_srcs) $(stdlib_rt) $(stdlib_types) $(stdlib_strings) $(stdlib_ascii)
 	@printf 'HAREC \t$@\n'
-	@mkdir -p $(HARECACHE)/fmt
-	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nfmt \
-		-t$(HARECACHE)/fmt/fmt.td $(stdlib_fmt_srcs)
+	@mkdir -p $(HARECACHE)/strconv
+	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nstrconv \
+		-t$(HARECACHE)/strconv/strconv.td $(stdlib_strconv_srcs)
 
-stdlib_stdlib_fmt=$(HARECACHE)/fmt/fmt.o
-hare_stdlib_deps+=$(stdlib_stdlib_fmt)
+# strings
+stdlib_strings_srcs= \
+	$(STDLIB)/strings/cstrings.ha \
+	$(STDLIB)/strings/dup.ha \
+	$(STDLIB)/strings/iter.ha \
+	$(STDLIB)/strings/utf8.ha 
+
+$(HARECACHE)/strings/strings.ssa: $(stdlib_strings_srcs) $(stdlib_rt) $(stdlib_encoding_utf8) $(stdlib_types)
+	@printf 'HAREC \t$@\n'
+	@mkdir -p $(HARECACHE)/strings
+	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nstrings \
+		-t$(HARECACHE)/strings/strings.td $(stdlib_strings_srcs)
+
+# types
+stdlib_types_srcs= \
+	$(STDLIB)/types/limits.ha \
+	$(STDLIB)/types/classes.ha \
+	$(STDLIB)/types/arch$(ARCH).ha 
+
+$(HARECACHE)/types/types.ssa: $(stdlib_types_srcs) $(stdlib_rt)
+	@printf 'HAREC \t$@\n'
+	@mkdir -p $(HARECACHE)/types
+	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Ntypes \
+		-t$(HARECACHE)/types/types.td $(stdlib_types_srcs)
 
 # rt
 testlib_rt_srcs= \
@@ -255,6 +255,33 @@ testlib_rt=$(TESTCACHE)/rt/rt.a
 testlib_start=$(TESTCACHE)/rt/start.o
 hare_testlib_deps+=$(testlib_rt)
 
+testlib_ascii=$(TESTCACHE)/ascii/ascii.o
+hare_testlib_deps+=$(testlib_ascii)
+
+testlib_bytes=$(TESTCACHE)/bytes/bytes.o
+hare_testlib_deps+=$(testlib_bytes)
+
+testlib_encoding_utf8=$(TESTCACHE)/encoding/utf8/encoding.utf8.o
+hare_testlib_deps+=$(testlib_encoding_utf8)
+
+testlib_fmt=$(TESTCACHE)/fmt/fmt.o
+hare_testlib_deps+=$(testlib_fmt)
+
+testlib_io=$(TESTCACHE)/io/io.o
+hare_testlib_deps+=$(testlib_io)
+
+testlib_os=$(TESTCACHE)/os/os.o
+hare_testlib_deps+=$(testlib_os)
+
+testlib_strconv=$(TESTCACHE)/strconv/strconv.o
+hare_testlib_deps+=$(testlib_strconv)
+
+testlib_strings=$(TESTCACHE)/strings/strings.o
+hare_testlib_deps+=$(testlib_strings)
+
+testlib_types=$(TESTCACHE)/types/types.o
+hare_testlib_deps+=$(testlib_types)
+
 # ascii
 testlib_ascii_srcs= \
 	$(STDLIB)/ascii/ctype.ha 
@@ -264,9 +291,6 @@ $(TESTCACHE)/ascii/ascii.ssa: $(testlib_ascii_srcs) $(testlib_rt)
 	@mkdir -p $(TESTCACHE)/ascii
 	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nascii \
 		-t$(TESTCACHE)/ascii/ascii.td $(testlib_ascii_srcs)
-
-testlib_testlib_ascii=$(TESTCACHE)/ascii/ascii.o
-hare_testlib_deps+=$(testlib_testlib_ascii)
 
 # bytes
 testlib_bytes_srcs= \
@@ -281,23 +305,27 @@ $(TESTCACHE)/bytes/bytes.ssa: $(testlib_bytes_srcs) $(testlib_rt)
 	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nbytes \
 		-t$(TESTCACHE)/bytes/bytes.td $(testlib_bytes_srcs)
 
-testlib_testlib_bytes=$(TESTCACHE)/bytes/bytes.o
-hare_testlib_deps+=$(testlib_testlib_bytes)
+# encoding::utf8
+testlib_encoding_utf8_srcs= \
+	$(STDLIB)/encoding/utf8/rune.ha \
+	$(STDLIB)/encoding/utf8/decode.ha \
+	$(STDLIB)/encoding/utf8/encode.ha 
 
-# types
-testlib_types_srcs= \
-	$(STDLIB)/types/limits.ha \
-	$(STDLIB)/types/classes.ha \
-	$(STDLIB)/types/arch$(ARCH).ha 
-
-$(TESTCACHE)/types/types.ssa: $(testlib_types_srcs) $(testlib_rt)
+$(TESTCACHE)/encoding/utf8/encoding.utf8.ssa: $(testlib_encoding_utf8_srcs) $(testlib_rt) $(testlib_types)
 	@printf 'HAREC \t$@\n'
-	@mkdir -p $(TESTCACHE)/types
-	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Ntypes \
-		-t$(TESTCACHE)/types/types.td $(testlib_types_srcs)
+	@mkdir -p $(TESTCACHE)/encoding/utf8
+	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nencoding::utf8 \
+		-t$(TESTCACHE)/encoding/utf8/encoding.utf8.td $(testlib_encoding_utf8_srcs)
 
-testlib_testlib_types=$(TESTCACHE)/types/types.o
-hare_testlib_deps+=$(testlib_testlib_types)
+# fmt
+testlib_fmt_srcs= \
+	$(STDLIB)/fmt/fmt.ha 
+
+$(TESTCACHE)/fmt/fmt.ssa: $(testlib_fmt_srcs) $(testlib_rt) $(testlib_io) $(testlib_os) $(testlib_strconv) $(testlib_strings) $(testlib_types)
+	@printf 'HAREC \t$@\n'
+	@mkdir -p $(TESTCACHE)/fmt
+	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nfmt \
+		-t$(TESTCACHE)/fmt/fmt.td $(testlib_fmt_srcs)
 
 # io
 testlib_io_srcs= \
@@ -314,57 +342,6 @@ $(TESTCACHE)/io/io.ssa: $(testlib_io_srcs) $(testlib_rt)
 	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nio \
 		-t$(TESTCACHE)/io/io.td $(testlib_io_srcs)
 
-testlib_testlib_io=$(TESTCACHE)/io/io.o
-hare_testlib_deps+=$(testlib_testlib_io)
-
-# encoding::utf8
-testlib_encoding_utf8_srcs= \
-	$(STDLIB)/encoding/utf8/rune.ha \
-	$(STDLIB)/encoding/utf8/decode.ha \
-	$(STDLIB)/encoding/utf8/encode.ha 
-
-$(TESTCACHE)/encoding/utf8/encoding.utf8.ssa: $(testlib_encoding_utf8_srcs) $(testlib_rt) $(testlib_testlib_types)
-	@printf 'HAREC \t$@\n'
-	@mkdir -p $(TESTCACHE)/encoding/utf8
-	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nencoding::utf8 \
-		-t$(TESTCACHE)/encoding/utf8/encoding.utf8.td $(testlib_encoding_utf8_srcs)
-
-testlib_testlib_encoding_utf8=$(TESTCACHE)/encoding/utf8/encoding.utf8.o
-hare_testlib_deps+=$(testlib_testlib_encoding_utf8)
-
-# strings
-testlib_strings_srcs= \
-	$(STDLIB)/strings/cstrings.ha \
-	$(STDLIB)/strings/dup.ha \
-	$(STDLIB)/strings/iter.ha \
-	$(STDLIB)/strings/utf8.ha 
-
-$(TESTCACHE)/strings/strings.ssa: $(testlib_strings_srcs) $(testlib_rt) $(testlib_testlib_encoding_utf8) $(testlib_testlib_types)
-	@printf 'HAREC \t$@\n'
-	@mkdir -p $(TESTCACHE)/strings
-	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nstrings \
-		-t$(TESTCACHE)/strings/strings.td $(testlib_strings_srcs)
-
-testlib_testlib_strings=$(TESTCACHE)/strings/strings.o
-hare_testlib_deps+=$(testlib_testlib_strings)
-
-# strconv
-testlib_strconv_srcs= \
-	$(STDLIB)/strconv/itos.ha \
-	$(STDLIB)/strconv/utos.ha \
-	$(STDLIB)/strconv/stou.ha \
-	$(STDLIB)/strconv/numeric.ha \
-	$(STDLIB)/strconv/stou+test.ha 
-
-$(TESTCACHE)/strconv/strconv.ssa: $(testlib_strconv_srcs) $(testlib_rt) $(testlib_testlib_types) $(testlib_testlib_strings) $(testlib_testlib_ascii)
-	@printf 'HAREC \t$@\n'
-	@mkdir -p $(TESTCACHE)/strconv
-	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nstrconv \
-		-t$(TESTCACHE)/strconv/strconv.td $(testlib_strconv_srcs)
-
-testlib_testlib_strconv=$(TESTCACHE)/strconv/strconv.o
-hare_testlib_deps+=$(testlib_testlib_strconv)
-
 # os
 testlib_os_srcs= \
 	$(STDLIB)/os/$(PLATFORM)/environ.ha \
@@ -376,25 +353,48 @@ testlib_os_srcs= \
 	$(STDLIB)/os/environ.ha \
 	$(STDLIB)/os/stdfd.ha 
 
-$(TESTCACHE)/os/os.ssa: $(testlib_os_srcs) $(testlib_rt) $(testlib_testlib_strings) $(testlib_testlib_types)
+$(TESTCACHE)/os/os.ssa: $(testlib_os_srcs) $(testlib_rt) $(testlib_strings) $(testlib_types)
 	@printf 'HAREC \t$@\n'
 	@mkdir -p $(TESTCACHE)/os
 	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nos \
 		-t$(TESTCACHE)/os/os.td $(testlib_os_srcs)
 
-testlib_testlib_os=$(TESTCACHE)/os/os.o
-hare_testlib_deps+=$(testlib_testlib_os)
+# strconv
+testlib_strconv_srcs= \
+	$(STDLIB)/strconv/itos.ha \
+	$(STDLIB)/strconv/utos.ha \
+	$(STDLIB)/strconv/stou.ha \
+	$(STDLIB)/strconv/numeric.ha \
+	$(STDLIB)/strconv/stou+test.ha 
 
-# fmt
-testlib_fmt_srcs= \
-	$(STDLIB)/fmt/fmt.ha 
-
-$(TESTCACHE)/fmt/fmt.ssa: $(testlib_fmt_srcs) $(testlib_rt) $(testlib_testlib_io) $(testlib_testlib_os) $(testlib_testlib_strconv) $(testlib_testlib_strings) $(testlib_testlib_types)
+$(TESTCACHE)/strconv/strconv.ssa: $(testlib_strconv_srcs) $(testlib_rt) $(testlib_types) $(testlib_strings) $(testlib_ascii)
 	@printf 'HAREC \t$@\n'
-	@mkdir -p $(TESTCACHE)/fmt
-	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nfmt \
-		-t$(TESTCACHE)/fmt/fmt.td $(testlib_fmt_srcs)
+	@mkdir -p $(TESTCACHE)/strconv
+	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nstrconv \
+		-t$(TESTCACHE)/strconv/strconv.td $(testlib_strconv_srcs)
 
-testlib_testlib_fmt=$(TESTCACHE)/fmt/fmt.o
-hare_testlib_deps+=$(testlib_testlib_fmt)
+# strings
+testlib_strings_srcs= \
+	$(STDLIB)/strings/cstrings.ha \
+	$(STDLIB)/strings/dup.ha \
+	$(STDLIB)/strings/iter.ha \
+	$(STDLIB)/strings/utf8.ha 
+
+$(TESTCACHE)/strings/strings.ssa: $(testlib_strings_srcs) $(testlib_rt) $(testlib_encoding_utf8) $(testlib_types)
+	@printf 'HAREC \t$@\n'
+	@mkdir -p $(TESTCACHE)/strings
+	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nstrings \
+		-t$(TESTCACHE)/strings/strings.td $(testlib_strings_srcs)
+
+# types
+testlib_types_srcs= \
+	$(STDLIB)/types/limits.ha \
+	$(STDLIB)/types/classes.ha \
+	$(STDLIB)/types/arch$(ARCH).ha 
+
+$(TESTCACHE)/types/types.ssa: $(testlib_types_srcs) $(testlib_rt)
+	@printf 'HAREC \t$@\n'
+	@mkdir -p $(TESTCACHE)/types
+	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Ntypes \
+		-t$(TESTCACHE)/types/types.td $(testlib_types_srcs)
 
