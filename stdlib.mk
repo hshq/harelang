7c7264d58d3cddc4ac8268dab8ddd0aa0aaeb5ee
Drew DeVault <sir@cmpwn.com>
2021-06-23 18:13:22 -0400

    net::dial: initial implementation for TCP
    Signed-off-by: Drew DeVault <sir@cmpwn.com>

--- stdlib.mk
+++ stdlib.mk
@@ -835,9 +835,9 @@ $(HARECACHE)/net/net.ssa: $(stdlib_net_srcs) $(stdlib_rt) $(stdlib_io) $(stdlib_
 # net::dial
 # net::dial
 stdlib_net_dial_srcs= \
+	$(STDLIB)/net/dial/registry.ha \
 	$(STDLIB)/net/dial/dial.ha \
 	$(STDLIB)/net/dial/ip.ha \
-	$(STDLIB)/net/dial/registry.ha \
 	$(STDLIB)/net/dial/resolve.ha
 
 $(HARECACHE)/net/dial/net_dial.ssa: $(stdlib_net_dial_srcs) $(stdlib_rt) $(stdlib_io) $(stdlib_net) $(stdlib_net_ip) $(stdlib_net_tcp) $(stdlib_net_udp) $(stdlib_net_dns)
@@ -2005,9 +2005,9 @@ $(TESTCACHE)/net/net.ssa: $(testlib_net_srcs) $(testlib_rt) $(testlib_io) $(test
 # net::dial
 # net::dial
 testlib_net_dial_srcs= \
+	$(STDLIB)/net/dial/registry.ha \
 	$(STDLIB)/net/dial/dial.ha \
 	$(STDLIB)/net/dial/ip.ha \
-	$(STDLIB)/net/dial/registry.ha \
 	$(STDLIB)/net/dial/resolve.ha
 
 $(TESTCACHE)/net/dial/net_dial.ssa: $(testlib_net_dial_srcs) $(testlib_rt) $(testlib_io) $(testlib_net) $(testlib_net_ip) $(testlib_net_tcp) $(testlib_net_udp) $(testlib_net_dns)
