2466d99e00acca49f4278c4343341d4d9138d8ea
Drew DeVault <sir@cmpwn.com>
2022-03-25 15:26:18 +0100

    hare::module2: update stdlib.mk

--- stdlib.mk
+++ stdlib.mk
@@ -1258,9 +1258,12 @@ $(HARECACHE)/hare/module/hare_module-any.ssa: $(stdlib_hare_module_any_srcs) $(s
 # hare::module2 (+any)
 stdlib_hare_module2_any_srcs= \
 	$(STDLIB)/hare/module2/context.ha \
-	$(STDLIB)/hare/module2/ident.ha
+	$(STDLIB)/hare/module2/error.ha \
+	$(STDLIB)/hare/module2/ident.ha \
+	$(STDLIB)/hare/module2/resolve.ha \
+	$(STDLIB)/hare/module2/scan.ha
 
-$(HARECACHE)/hare/module2/hare_module2-any.ssa: $(stdlib_hare_module2_any_srcs) $(stdlib_rt) $(stdlib_dirs_$(PLATFORM)) $(stdlib_os_$(PLATFORM)) $(stdlib_fs_$(PLATFORM)) $(stdlib_strings_$(PLATFORM)) $(stdlib_path_$(PLATFORM)) $(stdlib_fmt_$(PLATFORM)) $(stdlib_hare_ast_$(PLATFORM)) $(stdlib_errors_$(PLATFORM)) $(stdlib_strio_$(PLATFORM))
+$(HARECACHE)/hare/module2/hare_module2-any.ssa: $(stdlib_hare_module2_any_srcs) $(stdlib_rt) $(stdlib_dirs_$(PLATFORM)) $(stdlib_os_$(PLATFORM)) $(stdlib_fs_$(PLATFORM)) $(stdlib_strings_$(PLATFORM)) $(stdlib_path_$(PLATFORM)) $(stdlib_fmt_$(PLATFORM)) $(stdlib_hare_ast_$(PLATFORM)) $(stdlib_errors_$(PLATFORM)) $(stdlib_strio_$(PLATFORM)) $(stdlib_io_$(PLATFORM)) $(stdlib_hare_parse_$(PLATFORM))
 	@printf 'HAREC \t$@\n'
 	@mkdir -p $(HARECACHE)/hare/module2
 	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nhare::module2 \
@@ -3421,9 +3424,12 @@ $(TESTCACHE)/hare/module/hare_module-any.ssa: $(testlib_hare_module_any_srcs) $(
 # hare::module2 (+any)
 testlib_hare_module2_any_srcs= \
 	$(STDLIB)/hare/module2/context.ha \
-	$(STDLIB)/hare/module2/ident.ha
+	$(STDLIB)/hare/module2/error.ha \
+	$(STDLIB)/hare/module2/ident.ha \
+	$(STDLIB)/hare/module2/resolve.ha \
+	$(STDLIB)/hare/module2/scan.ha
 
-$(TESTCACHE)/hare/module2/hare_module2-any.ssa: $(testlib_hare_module2_any_srcs) $(testlib_rt) $(testlib_dirs_$(PLATFORM)) $(testlib_os_$(PLATFORM)) $(testlib_fs_$(PLATFORM)) $(testlib_strings_$(PLATFORM)) $(testlib_path_$(PLATFORM)) $(testlib_fmt_$(PLATFORM)) $(testlib_hare_ast_$(PLATFORM)) $(testlib_errors_$(PLATFORM)) $(testlib_strio_$(PLATFORM))
+$(TESTCACHE)/hare/module2/hare_module2-any.ssa: $(testlib_hare_module2_any_srcs) $(testlib_rt) $(testlib_dirs_$(PLATFORM)) $(testlib_os_$(PLATFORM)) $(testlib_fs_$(PLATFORM)) $(testlib_strings_$(PLATFORM)) $(testlib_path_$(PLATFORM)) $(testlib_fmt_$(PLATFORM)) $(testlib_hare_ast_$(PLATFORM)) $(testlib_errors_$(PLATFORM)) $(testlib_strio_$(PLATFORM)) $(testlib_io_$(PLATFORM)) $(testlib_hare_parse_$(PLATFORM))
 	@printf 'HAREC \t$@\n'
 	@mkdir -p $(TESTCACHE)/hare/module2
 	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nhare::module2 \
