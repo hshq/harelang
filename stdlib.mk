dcece9c02c5303b6bc4e074c85f255200419d07d
Drew DeVault <sir@cmpwn.com>
2022-03-27 11:43:53 +0200

    hare::module2: implement cache entry formatting

--- stdlib.mk
+++ stdlib.mk
@@ -1258,6 +1258,7 @@ $(HARECACHE)/hare/module/hare_module-any.ssa: $(stdlib_hare_module_any_srcs) $(s
 # hare::module2 (+any)
 stdlib_hare_module2_any_srcs= \
 	$(STDLIB)/hare/module2/abi.ha \
+	$(STDLIB)/hare/module2/cache.ha \
 	$(STDLIB)/hare/module2/context.ha \
 	$(STDLIB)/hare/module2/error.ha \
 	$(STDLIB)/hare/module2/ident.ha \
@@ -1265,7 +1266,7 @@ stdlib_hare_module2_any_srcs= \
 	$(STDLIB)/hare/module2/scan.ha \
 	$(STDLIB)/hare/module2/tags.ha
 
-$(HARECACHE)/hare/module2/hare_module2-any.ssa: $(stdlib_hare_module2_any_srcs) $(stdlib_rt) $(stdlib_dirs_$(PLATFORM)) $(stdlib_os_$(PLATFORM)) $(stdlib_fs_$(PLATFORM)) $(stdlib_strings_$(PLATFORM)) $(stdlib_path_$(PLATFORM)) $(stdlib_fmt_$(PLATFORM)) $(stdlib_hare_ast_$(PLATFORM)) $(stdlib_errors_$(PLATFORM)) $(stdlib_strio_$(PLATFORM)) $(stdlib_io_$(PLATFORM)) $(stdlib_hare_parse_$(PLATFORM)) $(stdlib_hare_lex_$(PLATFORM)) $(stdlib_crypto_sha256_$(PLATFORM)) $(stdlib_sort_$(PLATFORM)) $(stdlib_ascii_$(PLATFORM)) $(stdlib_hare_unparse_$(PLATFORM))
+$(HARECACHE)/hare/module2/hare_module2-any.ssa: $(stdlib_hare_module2_any_srcs) $(stdlib_rt) $(stdlib_dirs_$(PLATFORM)) $(stdlib_os_$(PLATFORM)) $(stdlib_fs_$(PLATFORM)) $(stdlib_strings_$(PLATFORM)) $(stdlib_path_$(PLATFORM)) $(stdlib_fmt_$(PLATFORM)) $(stdlib_hare_ast_$(PLATFORM)) $(stdlib_errors_$(PLATFORM)) $(stdlib_strio_$(PLATFORM)) $(stdlib_io_$(PLATFORM)) $(stdlib_hare_parse_$(PLATFORM)) $(stdlib_hare_lex_$(PLATFORM)) $(stdlib_crypto_sha256_$(PLATFORM)) $(stdlib_sort_$(PLATFORM)) $(stdlib_ascii_$(PLATFORM)) $(stdlib_hare_unparse_$(PLATFORM)) $(stdlib_time_$(PLATFORM))
 	@printf 'HAREC \t$@\n'
 	@mkdir -p $(HARECACHE)/hare/module2
 	@HARECACHE=$(HARECACHE) $(HAREC) $(HAREFLAGS) -o $@ -Nhare::module2 \
@@ -3426,6 +3427,7 @@ $(TESTCACHE)/hare/module/hare_module-any.ssa: $(testlib_hare_module_any_srcs) $(
 # hare::module2 (+any)
 testlib_hare_module2_any_srcs= \
 	$(STDLIB)/hare/module2/abi.ha \
+	$(STDLIB)/hare/module2/cache.ha \
 	$(STDLIB)/hare/module2/context.ha \
 	$(STDLIB)/hare/module2/error.ha \
 	$(STDLIB)/hare/module2/ident.ha \
@@ -3433,7 +3435,7 @@ testlib_hare_module2_any_srcs= \
 	$(STDLIB)/hare/module2/scan.ha \
 	$(STDLIB)/hare/module2/tags.ha
 
-$(TESTCACHE)/hare/module2/hare_module2-any.ssa: $(testlib_hare_module2_any_srcs) $(testlib_rt) $(testlib_dirs_$(PLATFORM)) $(testlib_os_$(PLATFORM)) $(testlib_fs_$(PLATFORM)) $(testlib_strings_$(PLATFORM)) $(testlib_path_$(PLATFORM)) $(testlib_fmt_$(PLATFORM)) $(testlib_hare_ast_$(PLATFORM)) $(testlib_errors_$(PLATFORM)) $(testlib_strio_$(PLATFORM)) $(testlib_io_$(PLATFORM)) $(testlib_hare_parse_$(PLATFORM)) $(testlib_hare_lex_$(PLATFORM)) $(testlib_crypto_sha256_$(PLATFORM)) $(testlib_sort_$(PLATFORM)) $(testlib_ascii_$(PLATFORM)) $(testlib_hare_unparse_$(PLATFORM))
+$(TESTCACHE)/hare/module2/hare_module2-any.ssa: $(testlib_hare_module2_any_srcs) $(testlib_rt) $(testlib_dirs_$(PLATFORM)) $(testlib_os_$(PLATFORM)) $(testlib_fs_$(PLATFORM)) $(testlib_strings_$(PLATFORM)) $(testlib_path_$(PLATFORM)) $(testlib_fmt_$(PLATFORM)) $(testlib_hare_ast_$(PLATFORM)) $(testlib_errors_$(PLATFORM)) $(testlib_strio_$(PLATFORM)) $(testlib_io_$(PLATFORM)) $(testlib_hare_parse_$(PLATFORM)) $(testlib_hare_lex_$(PLATFORM)) $(testlib_crypto_sha256_$(PLATFORM)) $(testlib_sort_$(PLATFORM)) $(testlib_ascii_$(PLATFORM)) $(testlib_hare_unparse_$(PLATFORM)) $(testlib_time_$(PLATFORM))
 	@printf 'HAREC \t$@\n'
 	@mkdir -p $(TESTCACHE)/hare/module2
 	@HARECACHE=$(TESTCACHE) $(HAREC) $(TESTHAREFLAGS) -o $@ -Nhare::module2 \
