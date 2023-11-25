0bfb86446fe627a941716c1e0f169ed2c69f5ca7
Lorenz (xha) <me@xha.li>
2023-11-25 15:18:02 +0100

    remove config.example.mk and add configs/<platform>.mk
    Signed-off-by: Lorenz (xha) <me@xha.li>

---
+++ configs/openbsd.mk
@@ -0,0 +1,43 @@
+# install locations
+PREFIX = /home/lorenz/bin
+BINDIR = $(PREFIX)/bin
+MANDIR = $(PREFIX)/share/man
+SRCDIR = $(PREFIX)/src
+STDLIB = $(SRCDIR)/hare/stdlib
+
+# variables used during build
+PLATFORM = openbsd
+ARCH = x86_64
+HAREFLAGS =
+HARECFLAGS =
+QBEFLAGS =
+ASFLAGS =
+LDLINKFLAGS = -z nobtcfi
+
+# commands used by the build script
+HAREC = harec
+QBE = qbe
+# OpenBSD: gas is in the binutils package. as from the base system is too old.
+AS = gas
+LD = cc
+SCDOC = scdoc
+
+# build locations
+HARECACHE = .cache
+BINOUT = .bin
+
+# variables that will be embedded in the binary with -D definitions
+HAREPATH = $(SRCDIR)/hare/stdlib:$(SRCDIR)/hare/third-party
+VERSION=$$(./scripts/version)
+
+AARCH64_AS=aarch64-gas
+AARCH64_CC=aarch64-cc
+AARCH64_LD=aarch64-cc
+
+RISCV64_AS=riscv64-gas
+RISCV64_CC=riscv64-cc
+RISCV64_LD=riscv64-cc
+
+X86_64_AS=gas
+X86_64_CC=cc
+X86_64_LD=cc
