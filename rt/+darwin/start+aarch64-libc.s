01f0a35a06b8fff430ce136e6cc4051c840aef73
hshq <hshqcn@qq.com>
2023-12-13 13:38:18 +0800

    merge

--- rt/+darwin/start+aarch64-libc.s
+++ rt/+darwin/start+aarch64-libc.s
@@ -4,15 +4,15 @@ _start:
 	mov 	x29, #0
 	mov 	x30, #0
 
-	; platformstart.ha:
+	; platformstart-libc.ha:
 	add		x0, x1, #-8
 
-	; platformstart+aarch64.ha 1:
+	; platformstart+aarch64-libc.ha 1:
 	; stp		x2, x3, [sp, #-16]!
 	; stp		x0, x1, [sp, #-16]!
 	; mov		x0, sp
 
-	; platformstart+aarch64.ha 2:
+	; platformstart+aarch64-libc.ha 2:
 
 	b 		_rt.start_darwin
 
