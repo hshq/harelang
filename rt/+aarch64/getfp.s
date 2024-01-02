a129bf33fda5f38248a33c7181752fb018b6bb4a
Drew DeVault <sir@cmpwn.com>
2024-01-02 13:24:40 +0100

    rt: remove backtrace functionality
    Implemented better in debug::
    
    Signed-off-by: Drew DeVault <sir@cmpwn.com>

--- rt/+aarch64/getfp.s
+++
@@ -1,6 +0,0 @@
-.section ".text.rt.getfp","ax"
-.global rt.getfp
-.type rt.getfp,@function
-rt.getfp:
-	mov x0, x29
-	ret
--- rt/+riscv64/getfp.s
+++
@@ -1,6 +0,0 @@
-.section ".text.rt.getfp","ax"
-.global rt.getfp
-.type rt.getfp,@function
-rt.getfp:
-	mv a0, fp
-	ret
