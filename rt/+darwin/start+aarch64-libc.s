1405140150be835f073b1a7c09f55a0330c73cec
hshq <hshqcn@qq.com>
2024-03-11 23:19:43 +0800

    Support compiling external libraries
    Building external libraries

--- rt/+darwin/start+aarch64-libc.s
+++ rt/+darwin/start+aarch64-libc.s
@@ -5,7 +5,7 @@ _start:
 	mov 	x30, #0
 
 	; platformstart-libc.ha:
-	add		x0, x1, #-8
+	sub		x0, x1, #8
 
 	; platformstart+aarch64-libc.ha 1:
 	; stp		x2, x3, [sp, #-16]!
@@ -16,21 +16,4 @@ _start:
 
 	b 		_rt.start_darwin
 
-
-.private_extern ___init_array_start
-___init_array_start = section$start$__DATA$.init_array
-
-.private_extern ___init_array_end
-___init_array_end = section$end$__DATA$.init_array
-
-.private_extern ___fini_array_start
-___fini_array_start = section$start$__DATA$.fini_array
-
-.private_extern ___fini_array_end
-___fini_array_end = section$end$__DATA$.fini_array
-
-.private_extern ___test_array_start
-___test_array_start = section$start$__DATA$.test_array
-
-.private_extern ___test_array_end
-___test_array_end = section$end$__DATA$.test_array
+.include "start-libc.s"
--- rt/+darwin/start+x86_64-libc.s
+++ rt/+darwin/start+x86_64-libc.s
@@ -6,21 +6,4 @@ _start:
 	leaq -8(%rsi), %rdi
 	call _rt.start_darwin
 
-
-.private_extern ___init_array_start
-___init_array_start = section$start$__DATA$.init_array
-
-.private_extern ___init_array_end
-___init_array_end = section$end$__DATA$.init_array
-
-.private_extern ___fini_array_start
-___fini_array_start = section$start$__DATA$.fini_array
-
-.private_extern ___fini_array_end
-___fini_array_end = section$end$__DATA$.fini_array
-
-.private_extern ___test_array_start
-___test_array_start = section$start$__DATA$.test_array
-
-.private_extern ___test_array_end
-___test_array_end = section$end$__DATA$.test_array
+.include "start-libc.s"
