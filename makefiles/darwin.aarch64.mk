63dc2df16e319fc6506ab5ca440a979d9beecccc
hshq <hshqcn@qq.com>
2024-10-03 01:01:10 +0800

    merge

--- makefiles/darwin.aarch64.mk
+++ makefiles/darwin.aarch64.mk
@@ -48,7 +48,7 @@ $(HARECACHE)/std/errors.ssa: $(errors_ha) $(HARECACHE)/std/rt.td
 	@mkdir -p -- "$(HARECACHE)/std"
 	@$(TDENV) $(HAREC) $(HARECFLAGS) -o $@ -t $(HARECACHE)/std/errors.td.tmp -N errors $(errors_ha)
 
-io_ha = io/+darwin/dup.ha io/+darwin/mmap.ha io/+darwin/platform_file.ha io/+darwin/vector.ha io/arch+aarch64.ha io/copy.ha io/drain.ha io/empty.ha io/file.ha io/handle.ha io/limit.ha io/stream.ha io/tee.ha io/types.ha io/util.ha io/zero.ha
+io_ha = io/+darwin/dup.ha io/+darwin/mmap.ha io/+darwin/platform_file.ha io/+darwin/sync.ha io/+darwin/vector.ha io/arch+aarch64.ha io/copy.ha io/drain.ha io/empty.ha io/file.ha io/handle.ha io/limit.ha io/stream.ha io/tee.ha io/types.ha io/util.ha io/zero.ha
 $(HARECACHE)/std/io.ssa: $(io_ha) $(HARECACHE)/std/bytes.td $(HARECACHE)/std/errors.td $(HARECACHE)/std/rt.td $(HARECACHE)/std/types.td
 	@mkdir -p -- "$(HARECACHE)/std"
 	@$(TDENV) $(HAREC) $(HARECFLAGS) -o $@ -t $(HARECACHE)/std/io.td.tmp -N io $(io_ha)
